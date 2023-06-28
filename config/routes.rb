f872d81c5a4dd0e92792962dbc8083e7fec4eb40
Caleb Nwaizu <calebchris000@gmail.com>
2023-06-28 07:42:44 +0100

    Change default route to food

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :foods
-  root 'user#index'
+  root 'foods#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
