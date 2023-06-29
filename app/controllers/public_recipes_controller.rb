fb9a60e922685bdf93c74d475ee3bbf0195d08ac
Caleb Nwaizu <calebchris000@gmail.com>
2023-06-29 10:42:39 +0100

    Merge

---
+++ app/controllers/public_recipes_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class PublicRecipesController < ApplicationController
+  load_and_authorize_resource
+  def index
+    @recipes = Recipe.where('public = ? OR user_id = ?', true, current_user.id)
+  end
+end
---
+++ test/controllers/public_recipes_controller_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class PublicRecipesControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    get public_recipes_index_url
+    assert_response :success
+  end
+end
