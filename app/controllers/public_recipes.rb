d76b9e94bcd54cd891f901ef23ca8526f059801d
Lornakaboro <lornakaboro@gmail.com>
2023-06-29 10:21:04 +0300

    Remove unncecesary files

--- app/controllers/public_recipes.rb
+++
@@ -1,6 +0,0 @@
-class PublicRecipesController < ApplicationController
-    load_and_authorize_resource
-    def index
-      @recipes = Recipe.where('public = ? OR user_id = ?', true, current_user.id)
-    end
-  end
--- app/models/food_recipe.rb
+++
@@ -1,6 +0,0 @@
-class FoodRecipe < ApplicationRecord
-  belongs_to :recipe
-  belongs_to :food
-
-  validates :quantity, presence: true
-end
