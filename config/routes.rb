c236e317294b2df6272481af479e37ed43be3ad4
Lornakaboro <lornakaboro@gmail.com>
2023-06-29 10:11:18 +0300

    Modiffy routes for the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'recipe_foods/new'
+  get 'recipe_foods/create'
+  get 'recipe_foods/edit'
+  get 'recipe_foods/destroy'
+  get 'public_recipes/index'
+  resources :recipes
+  resources :foods
   devise_for :users
   resources :users
-  root 'user#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'users#index'
 end
