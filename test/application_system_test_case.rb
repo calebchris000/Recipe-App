dda2e9f8f872d59fcc071ec689b3ccda6d3d445d
Lornakaboro <lornakaboro@gmail.com>
2023-07-01 11:52:45 +0300

    Fix linter errors

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,7 +5,8 @@ CHROMEDRIVER_URL = "http://#{WINDOWS_HOST}:9515/".freeze
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
   driven_by :selenium_remote_chrome
-  Capybara.register_driver :selenium_remote_chrome do |app|s
+  Capybara.register_driver :selenium_remote_chrome do |app|
+    s
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--start-maximized')
 
